f9e68a6c3b5a708ae426ba0c0fafc5295f07628a
Sayed Miah <zcapmia@ucl.ac.uk>
2018-10-25 15:31:11 +0100

    module4/numericalreader.java added analysedata method, not working

--- PHAS0056/src/module4/NumericalReader.java
+++ PHAS0056/src/module4/NumericalReader.java
@@ -1,6 +1,7 @@
 package module4;
 import java.io.*;
 import java.net.URL;
+import java.util.Scanner;
 
 public class NumericalReader {
 	
@@ -21,16 +22,48 @@ public class NumericalReader {
 	
 	void analysisStart(String dataFile) throws IOException {
 		File f = new File(dataFile);
-		int minValue,maxValue,nValues,sumOfValues;
+		double minValue, maxValue, nValues= 0, sumOfValue = 0;
 	}
 	
-	void analyseData(String line) {
+	void analyseData(String line) throws NumberFormatException {
+		line.replaceAll("\\s+","");
+		line.replaceAll("\\","");
+		line.trim();
+		FileWriter fw = new FileWriter(dataFile);
+		BufferedWriter bw = new BufferedWriter(fw);
+		PrintWriter pw = new PrintWriter(bw);
+		Scanner s = new Scanner(line);
+		while (s.hasNext()) {
+			String token = s.next();
+			double x = Double.parseDouble(token);
+			pw.println(x);
+			sumOfValues += x;
+			nValues += 1;
+			if (x < minValue || minValue == null) {
+				minValue = x;
+			}
+			if (x > maxValue || maxValue == null) {
+				maxValue = x;
+			}
+		}
+		
+	}
+	
+	void analysisEnd() {
+		System.out.println("The minimum value is " + minValue);
+		System.out.println("The maxiumum value is " + maxValue);
+		System.out.println("The number of values is " +nValues);
+		System.out.println("The sum of all the values is " + sumOfValues);
 		
 	}
 
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			getStringFromKeyboard();
+		} catch (IOException e) {
+			//
+		}
 
 	}
 
