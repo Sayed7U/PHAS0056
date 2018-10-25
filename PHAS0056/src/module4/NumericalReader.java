1bb3d04929f35dca4ed53161f89712c3515c4d70
Sayed Miah <zcapmia@ucl.ac.uk>
2018-10-25 16:54:27 +0100

    module4/numericalreader.java define variables are global private

--- PHAS0056/src/module4/NumericalReader.java
+++ PHAS0056/src/module4/NumericalReader.java
@@ -4,35 +4,41 @@ import java.net.URL;
 import java.util.Scanner;
 
 public class NumericalReader {
-	
+	private Double minValue, maxValue,sumOfValues;
+	private int nValues;
+	private File outputFile;
+
 	public static String getStringFromKeyboard() throws IOException {
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
 		System.out.print("Please type something!");
 		String s = br.readLine();
-		if (s.length() == 0) {
-			s = System.getProperty("user.home");
-		}
+		//if (s.length() == 0) {
+		//	s = System.getProperty("user.home");
+		//	throw new Exception("No input given, resulting to default.");
+		//}
 		return s;
 	}
-	
+
 	public BufferedReader brFromURL(String urlName) throws IOException {
 		URL u = new URL(urlName);
 		InputStream is = u.openStream();
 		InputStreamReader isr = new InputStreamReader(is);
 		return new BufferedReader(isr);
 	}
-	
+
 	void analysisStart(String dataFile) throws IOException {
-		File f = new File(dataFile);
-		double minValue, maxValue, nValues= 0, sumOfValue = 0;
+		outputFile = new File(dataFile);
+		minValue = null;
+		maxValue = null;
+		nValues = 0;
+		sumOfValues = 0.0;
 	}
-	
-	void analyseData(String line) throws NumberFormatException {
+
+	void analyseData(String line) throws IOException {
 		line.replaceAll("\\s+","");
-		line.replaceAll("\\","");
 		line.trim();
-		FileWriter fw = new FileWriter(dataFile);
+		FileWriter fw = new FileWriter(outputFile);
 		BufferedWriter bw = new BufferedWriter(fw);
 		PrintWriter pw = new PrintWriter(bw);
 		Scanner s = new Scanner(line);
@@ -49,27 +55,25 @@ public class NumericalReader {
 				maxValue = x;
 			}
 		}
-		
+		s.close();
+		pw.close();
+
 	}
-	
+
 	void analysisEnd() {
 		System.out.println("The minimum value is " + minValue);
 		System.out.println("The maxiumum value is " + maxValue);
 		System.out.println("The number of values is " +nValues);
 		System.out.println("The sum of all the values is " + sumOfValues);
-		
+
 	}
 
 
 	public static void main(String[] args) {
 		try {
 			String saveDir = getStringFromKeyboard();
-			String saveFile = (saveDir + File.separator + dataFile);
-		} catch (IOException e) {
-			//
-		}
-		NumericalReader nr = new NumericalReader();
-		try {
+			NumericalReader nr = new NumericalReader();
+			String saveFile = (saveDir + File.separator + "numbers1.txt");
 			BufferedReader reader = nr.brFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_data1.txt");
 			String line = "";
 			nr.analysisStart(saveFile);
@@ -79,8 +83,9 @@ public class NumericalReader {
 			nr.analysisEnd();
 		} catch (IOException e) {
 			//
-		}
-		
+		}// catch (Exception e) {
+			//System.out.println(e);
+		//}
 	}
 
 }
