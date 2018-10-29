a9f41c75fbb177c77120ac13b28b586ade9687d2
Sayed Miah <zcapmia@ucl.ac.uk>
2018-10-29 15:05:46 +0000

    MODULE 4 FINAL

--- PHAS0056/src/module4/NumericalReader.java
+++ PHAS0056/src/module4/NumericalReader.java
@@ -8,15 +8,16 @@ public class NumericalReader {
 	private int nValues;
 	private File outputFile;
 
+	
+
 	public static String getStringFromKeyboard() throws IOException {
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
-		System.out.print("Please type something!");
+		System.out.print("Please enter the path for the file");
 		String s = br.readLine();
-		//if (s.length() == 0) {
-		//	s = System.getProperty("user.home");
-		//	throw new Exception("No input given, resulting to default.");
-		//}
+		if (s.length() == 0) {
+			s = System.getProperty("user.home");
+		}
 		return s;
 	}
 
@@ -34,38 +35,38 @@ public class NumericalReader {
 		nValues = 0;
 		sumOfValues = 0.0;
 	}
-
+	
 	void analyseData(String line) throws IOException {
-		line.replaceAll("\\s+","");
-		line.trim();
-		FileWriter fw = new FileWriter(outputFile);
+		FileWriter fw = new FileWriter(outputFile,true);
 		BufferedWriter bw = new BufferedWriter(fw);
 		PrintWriter pw = new PrintWriter(bw);
+		line = line.trim();
 		Scanner s = new Scanner(line);
-		while (s.hasNext()) {
-			String token = s.next();
-			double x = Double.parseDouble(token);
-			pw.println(x);
-			sumOfValues += x;
-			nValues += 1;
-			if (minValue == null || x < minValue) {
-				minValue = x;
-			}
-			if (maxValue == null || x > maxValue) {
-				maxValue = x;
+		if (line.length() != 0 && !line.startsWith("c") && !line.startsWith("x")) {
+			while (s.hasNext()) {
+				String token = s.next();
+				double x = Double.parseDouble(token);
+				pw.println(x);
+				System.out.println(x);
+				sumOfValues += x;
+				nValues += 1;
+				if (minValue == null || x < minValue) {
+					minValue = x;
+				}
+				if (maxValue == null || x > maxValue) {
+					maxValue = x;
+				}
 			}
 		}
 		s.close();
 		pw.close();
-
 	}
 
 	void analysisEnd() {
 		System.out.println("The minimum value is " + minValue);
 		System.out.println("The maxiumum value is " + maxValue);
-		System.out.println("The number of values is " +nValues);
+		System.out.println("The number of values is " + nValues);
 		System.out.println("The sum of all the values is " + sumOfValues);
-
 	}
 
 
@@ -79,11 +80,26 @@ public class NumericalReader {
 			nr.analysisStart(saveFile);
 			while ((line = reader.readLine()) != null) {
 				nr.analyseData(line);
+
 			}
 			nr.analysisEnd();
+			
+			NumericalReader nr2 = new NumericalReader();
+			String saveFile2 = (saveDir + File.separator + "numbers2.txt");
+			BufferedReader reader2 = nr2.brFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_data2.txt");
+			String line2 = "";
+			nr2.analysisStart(saveFile2);
+			while ((line2 = reader2.readLine()) != null) {
+				nr2.analyseData(line2);
+
+			}
+			nr2.analysisEnd();
 		} catch (IOException e) {
 			//
-		}// catch (Exception e) {
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		// catch (Exception e) {
 			//System.out.println(e);
 		//}
 	}
