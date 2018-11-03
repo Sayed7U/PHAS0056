209f5ab5c4e85512677d320e9293b9874477af68
Sayed Miah <zcapmia@ucl.ac.uk>
2018-11-03 11:38:50 +0000

    module5/TestSquarematrix completed with comments

--- PHAS0056/src/module5/TestSquareMatrix.java
+++ PHAS0056/src/module5/TestSquareMatrix.java
@@ -3,13 +3,30 @@ package module5;
 public class TestSquareMatrix {
 	public static void main(String[] args) {
 		try {
-			SquareMatrix s = new SquareMatrix(new double[][] {{1,2,3},{2,3,3},{3,1,2}});
-			SquareMatrix s2 = SquareMatrix.unitMatrix(3);
-			System.out.println(SquareMatrix.multiply(s, s2));
-			System.out.println(s);
+			
+			//defining the matrices
+			SquareMatrix A = new SquareMatrix(new double[][] {{2,1,0},{0,1,0},{-1,0,2}});
+			SquareMatrix B = new SquareMatrix(new double[][] {{1,3,1},{0,2,0},{1,0,-1}});
+			SquareMatrix C = new SquareMatrix(new double[][] {{2,3},{3,4}});
+			SquareMatrix D = new SquareMatrix(new double[][] {{-4,3},{3,-2}});
+			
+			System.out.println("A + B = \n" + SquareMatrix.add(A, B));
+			System.out.println("A - B = \n" + SquareMatrix.subtract(A, B));
+			SquareMatrix AB = SquareMatrix.multiply(A, B);
+			SquareMatrix BA = SquareMatrix.multiply(B, A);
+			System.out.println("AB = \n" + AB);
+			System.out.println("BA = \n" + BA);
+			System.out.println("The commutator [A,B] = \n" + SquareMatrix.subtract(AB,BA));
+			
+			SquareMatrix CD = SquareMatrix.multiply(C, D);
+			System.out.println("CD = \n" + CD);
+			System.out.println("is CD equal to the 2x2 identity? : " + CD.equals(SquareMatrix.unitMatrix(2)));
+			
 		} catch (Exception e) {
 			System.out.println(e);
 		}
+		
+		//checking if the 
 	}
 	
 
