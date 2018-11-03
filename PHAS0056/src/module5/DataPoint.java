e2f97ec2a2caf0b419bd783f532f32a379a1511e
Sayed Miah <zcapmia@ucl.ac.uk>
2018-11-03 12:08:39 +0000

    module5/theory.java added the constructor and main y func

--- PHAS0056/src/module5/DataPoint.java
+++ PHAS0056/src/module5/DataPoint.java
@@ -1,9 +1,6 @@
 package module5;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URL;
 import java.util.Scanner;
 
---
+++ PHAS0056/src/module5/Theory.java
@@ -0,0 +1,15 @@
+package module5;
+
+public class Theory {
+	
+	private int n;
+	
+	public Theory(int n1) {
+		this.n = n1;
+	}
+	
+	public double y(double x) {
+		return Math.pow(x, this.n);
+	}
+
+}
