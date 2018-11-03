510908d3b8ac46cb9e254af3a8c7748280a94c1a
Sayed Miah <zcapmia@ucl.ac.uk>
2018-11-03 12:22:05 +0000

    module5/datanalysis datafromurl returns an arraylist of datapoints

--- PHAS0056/src/module5/DataAnalysis.java
+++ PHAS0056/src/module5/DataAnalysis.java
@@ -1,13 +1,12 @@
 package module5;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URL;
 import java.util.*;
 
 public class DataAnalysis {
-	public static ArrayList dataFromURL(String url) {
+	public static ArrayList<DataPoint> dataFromURL(String url) throws IOException {
+		ArrayList<DataPoint> mypoints = new ArrayList<DataPoint>();
 		URL u = new URL(url); //creating URL object
 		InputStream is = u.openStream(); //opening the URL object 
 		InputStreamReader isr = new InputStreamReader(is); //reading the resulting input stream
@@ -16,15 +15,15 @@ public class DataAnalysis {
 		while ((line = br.readLine()) != null) {
 			line = line.trim();
 			Scanner s = new Scanner(line);
-			int i = 0;
 			while (s.hasNext()) {
-				this.x[i] = Double.parseDouble(s.next());
-				this.y[i] = Double.parseDouble(s.next());
-				this.ey[i] = Double.parseDouble(s.next());
-				i++;
+				DataPoint dp = new DataPoint(Double.parseDouble(s.next())
+						,Double.parseDouble(s.next())
+						,Double.parseDouble(s.next()));
+				mypoints.add(dp);
 			}
 			s.close();
 		}
+		return mypoints;
 	}
 
 	public static void main(String[] args) {
