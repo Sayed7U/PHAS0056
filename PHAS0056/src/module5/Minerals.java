7b775567dfd35031745025b2550a848a5d2a1f5f
sayed <sayed@10.97.237.198>
2018-11-11 15:15:12 +0000

    module5/minerals fixed the locationdatafromurl error and added smallmass

--- PHAS0056/src/module5/Minerals.java
+++ PHAS0056/src/module5/Minerals.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class Minerals {
 
 	public static HashMap<Integer, Double> massDataFromURL(String url) throws IOException {
-		HashMap<Integer, Double> mass = new HashMap<Integer, Double>();
+		HashMap<Integer, Double> massMap = new HashMap<Integer, Double>();
 		URL u = new URL(url); //creating URL object
 		InputStream is = u.openStream(); //opening the URL object 
 		InputStreamReader isr = new InputStreamReader(is); //reading the resulting input stream
@@ -17,15 +17,15 @@ public class Minerals {
 			line = line.trim(); //gets rid of white-space preceding and following a line
 			Scanner s = new Scanner(line);
 			while (s.hasNext()) {
-				mass.put(Integer.parseInt(s.next()),Double.parseDouble(s.next()));
+				massMap.put(Integer.parseInt(s.next()),Double.parseDouble(s.next()));
 			}
 			s.close(); //closing the scanner
 		}
-		return mass;
+		return massMap;
 	}
 	
 	public static HashMap<Integer, String> locationDataFromURL(String url) throws IOException {
-		HashMap<Integer, String> location = new HashMap<Integer, String>();
+		HashMap<Integer, String> locationMap = new HashMap<Integer, String>();
 		URL u = new URL(url); //creating URL object
 		InputStream is = u.openStream(); //opening the URL object 
 		InputStreamReader isr = new InputStreamReader(is); //reading the resulting input stream
@@ -35,20 +35,21 @@ public class Minerals {
 			line = line.trim(); //gets rid of white-space preceding and following a line
 			Scanner s = new Scanner(line);
 			while (s.hasNext()) {
-				location.put(s.nextInt(),s.next());
+				String location = s.next();
+				locationMap.put(Integer.parseInt(s.next()),location);
 			}
 			s.close(); //closing the scanner
 		}
-		return location;
+		return locationMap;
 	}
 	
 	public static String largestMass(HashMap<Integer,Double> m, HashMap<Integer,String> l) throws IOException {
-		double lmass = 0;
+		double lMass = 0;
 		int code = -1;
 		String location = "";
 		for (Map.Entry<Integer,Double> entry : m.entrySet()) {
-			if (entry.getValue() > lmass) {
-				lmass = entry.getValue();
+			if (entry.getValue() > lMass) {
+				lMass = entry.getValue();
 				code = entry.getKey();
 			}
 		}
@@ -58,15 +59,40 @@ public class Minerals {
 			}
 		}
 		return ("The sample with the largest mass: \n"
-				+ "Code Number: " + code + "/n" + 
-				"Mass: " + lmass + "\n" + 
-				"Location: " + location);
+				+ "Code Number: " + code + "\n" + 
+				"Mass: " + lMass + "\n" + 
+				"Location: " + location + "\n");
+	}
+	
+	public static String SmallestMass(HashMap<Integer,Double> m, HashMap<Integer,String> l) throws IOException {
+		double sMass = 0;
+		int code = -1;
+		String location = "";
+		for (Map.Entry<Integer,Double> entry : m.entrySet()) {
+			if (sMass == 0) {
+				sMass = entry.getValue();
+			}
+			if (entry.getValue() < sMass) {
+				sMass = entry.getValue();
+				code = entry.getKey();
+			}
+		}
+		for (Map.Entry<Integer, String> entry : l.entrySet()) {
+			if (entry.getKey() == code) {
+				location = entry.getValue();
+			}
+		}
+		return ("The sample with the smallest mass: \n"
+				+ "Code Number: " + code + "\n" + 
+				"Mass: " + sMass + "\n" + 
+				"Location: " + location + "\n");
 	}
 	public static void main(String[] args) {
 		try {
 			HashMap<Integer, Double> massData = massDataFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module5/module5-samples.txt");
 			HashMap<Integer, String> locationData = locationDataFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module5/module5-locations.txt");
 			System.out.println(largestMass(massData,locationData));
+			System.out.println(SmallestMass(massData,locationData));
 		} catch (IOException e) {
 			System.out.println("Problem: "+e.getMessage());
 		}
