3524b03ab55363ce6286e90bcfc2072fea7d888c
Sayed Miah <zcapmia@ucl.ac.uk>
2018-12-09 20:07:37 +0000

    module 8, countdowntask waiting

---
+++ PHAS0056/src/module8/CountdownTask.java
@@ -0,0 +1,23 @@
+package module8;
+
+public class CountdownTask implements Runnable {
+
+	private int sec;
+
+	public CountdownTask(int seconds) {
+		this.sec = seconds;
+	}
+
+	public void run() {
+		try {
+			while (sec >= 0) {
+				System.out.println(sec);
+				wait(1000);
+				sec -= 1;
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+}
